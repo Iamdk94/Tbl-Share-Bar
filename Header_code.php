71bf5ba08bb8500136eadad865c614791808f9e0
Deepak kumar sah <Deepakkumarsah58@gmail.com>
2017-10-17 00:21:44 +0530

    Update Header_code.php

--- Header_code.php
+++ Header_code.php
@@ -1,1 +1,14 @@
+# for Shar Bar only
+#add the code below 
+
+
 <script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>
+
+# For Google Analytics only 
+#add scripts below 
+
+
+<script async custom-element="amp-analytics"
+src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+
+#If you want to add both then use both codes
