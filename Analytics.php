e85fda7316343bae3140444daf4d6420f085b05b
Deepak kumar sah <Deepakkumarsah58@gmail.com>
2017-10-17 00:24:22 +0530

    Create Analytics.php

---
+++ Analytics.php
@@ -0,0 +1,15 @@
+<amp-analytics type="googleanalytics">
+<script type="application/json">
+{
+  "vars": {
+    "account": "UA-XXXXX-Y"
+  },
+  "triggers": {
+    "trackPageview": {
+      "on": "visible",
+      "request": "pageview"
+    }
+  }
+}
+</script>
+</amp-analytics>
